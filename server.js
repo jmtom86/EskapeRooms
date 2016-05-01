e3d7e3b5dd6b852462919ca70eb618654ff57a00
Jordan <jmtom86@yahoo.com>
2016-05-01 12:52:31 -0700

    added initial folders and files

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, './client')));
+app.listen(8000, function(){
+	console.log('Website on: 8000');
+})
